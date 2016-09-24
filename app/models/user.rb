589615b3fccd5a05f93f0be2aa0c93e521fd045d
Max Metcalfe <m.maxmetcalfe@gmail.com>
2016-09-24 15:15:37 -0700

    Fixing users model error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
-      user.racer_id = 1
-      user.admin = False
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
