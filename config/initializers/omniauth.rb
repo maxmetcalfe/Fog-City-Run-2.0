00510c1ab63a686c2466e085878ad0be7e008613
Max Metcalfe <m.maxmetcalfe@gmail.com>
2016-10-15 13:56:26 -0700

    Fixing OmniAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,17 @@
 OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1013683108730693', '75216b928ca28618a63893a86da2c801'
+PROD_FACEBOOK_APP_ID = '1013683108730693'
+PROD_FACEBOOK_APP_SECRET = '75216b928ca28618a63893a86da2c801'
+
+DEV_FACEBOOK_APP_ID = '678283658991535'
+DEV_FACEBOOK_APP_SECRET = '330d4ab8e7df5f542ad8427824d20e30'
+
+if Rails.env.production?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, PROD_FACEBOOK_APP_ID, PROD_FACEBOOK_APP_SECRET
+  end
+elsif Rails.env.development?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, DEV_FACEBOOK_APP_ID, DEV_FACEBOOK_APP_SECRET
+  end
 end
--- config/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1013683108730693', '75216b928ca28618a63893a86da2c801'
-end
