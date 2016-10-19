c3150cf3b9b06e0c00dcb6be84c2aaadb66a1f2f
Max Metcalfe <m.maxmetcalfe@gmail.com>
2016-10-19 15:11:59 -0700

    Set session after singup and go to racers_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "You signed up successfully"
-      redirect_to  login_path
+      redirect_to racers_path
     else
       render 'new'
     end
