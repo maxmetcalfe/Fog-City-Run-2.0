26a369502d87a13f061b60c9a56d36cd3bcc7f6c
Max Metcalfe <m.maxmetcalfe@gmail.com>
2016-11-02 16:01:25 -0700

    Shop: Changing number of required races to 15

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,7 +18,7 @@ class PagesController < ApplicationController
       @user_order = Order.where(:user_id => current_user.id).first
       # Only racers with 30 or more races can place an order.
       @user_shop_blocked = false
-      if Result.where(:racer_id => current_user.racer_id).count <= 30
+      if Result.where(:racer_id => current_user.racer_id).count <= 15
         @user_shop_blocked = true
       end
     end
