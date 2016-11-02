61c8c4fbebc2fac979577c107aaee1d738083ef2
Max Metcalfe <m.maxmetcalfe@gmail.com>
2016-11-01 23:22:44 -0700

    Fix login bugs for Shop

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,11 +14,13 @@ class PagesController < ApplicationController
   end
 
   def shop
-    @user_order = Order.where(:user_id => current_user.id).first
-    # Only racers with 30 or more races can place an order.
-    @user_shop_blocked = false
-    if Result.where(:racer_id => current_user.racer_id).count <= 30
-      @user_shop_blocked = true
+    if current_user
+      @user_order = Order.where(:user_id => current_user.id).first
+      # Only racers with 30 or more races can place an order.
+      @user_shop_blocked = false
+      if Result.where(:racer_id => current_user.racer_id).count <= 30
+        @user_shop_blocked = true
+      end
     end
   end
 
