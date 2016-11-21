e437191ceb0855c02e8f49e32646a6e869982e7c
Max Metcalfe <m.maxmetcalfe@gmail.com>
2016-11-21 16:45:03 -0500

    Turn on full error messages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   config.action_mailer.raise_delivery_errors = true
