0efb0bcab6db56fbb2556b69744a90a789eae610
Max Metcalfe <m.maxmetcalfe@gmail.com>
2016-12-19 20:50:38 -0800

    Fixing javascript bug

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,95 +1,82 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
-// global object
-T = {} ;
-T.timerDiv = document.getElementById('timer');
+$( document ).ready(function() {
+  // global object
+  T = {} ;
+  T.timerDiv = document.getElementById('timer');
+});
 
 function displayTimer() {
-// initilized all local variables:
-var hours='00', minutes='00',
-miliseconds=0, seconds='01',
-time = '',
-timeNow = new Date().getTime();
+  // initilized all local variables:
+  var hours='00', minutes='00',
+  miliseconds=0, seconds='00',
+  time = '',
+  timeNow = new Date().getTime(); // timestamp (miliseconds)
 
-T.difference = timeNow - T.timerStarted;
+  T.difference = timeNow - T.timerStarted;
 
-// seconds
-if(T.difference > 1000) {
-seconds = Math.floor(T.difference / 1000);
-if (seconds > 60) {
-seconds = seconds % 60;
-}
-if(seconds < 10) {
-seconds = '0'+String(seconds);
-}
-}
+  // milliseconds
+  if(T.difference > 10) {
+    miliseconds = Math.floor((T.difference % 1000) / 10);
+    if(miliseconds < 10) {
+      miliseconds = '0'+String(miliseconds);
+    }
+  }
+  // seconds
+  if(T.difference > 1000) {
+    seconds = Math.floor(T.difference / 1000);
+    if (seconds > 60) {
+      seconds = seconds % 60;
+    }
+    if(seconds < 10) {
+      seconds = '0'+String(seconds);
+    }
+  }
 
-// minutes
-if(T.difference > 60000) {
-minutes = Math.floor(T.difference/60000);
-if (minutes > 60) {
-minutes = minutes % 60;
-}
-if(minutes < 10) {
-minutes = '0'+String(minutes);
-}
-}
+  // minutes
+  if(T.difference > 60000) {
+    minutes = Math.floor(T.difference/60000);
+    if (minutes > 60) {
+      minutes = minutes % 60;
+    }
+    if(minutes < 10) {
+      minutes = '0'+String(minutes);
+    }
+  }
 
-// hours
-if(T.difference > 3600000) {
-hours = Math.floor(T.difference/3600000);
-// if (hours > 24) {
-// 	hours = hours % 24;
-// }
-if(hours < 10) {
-hours = '0'+String(hours);
-}
-}
+  // hours
+  if(T.difference > 3600000) {
+    hours = Math.floor(T.difference/3600000);
+    // if (hours > 24) {
+    // 	hours = hours % 24;
+    // }
+    if(hours < 10) {
+      hours = '0'+String(hours);
+    }
+  }
 
-time  =  hours   + ':'
-time += minutes + ':'
-time += seconds;
+  time  =  hours   + ':'
+  time += minutes + ':'
+  time += seconds + ':'
+  time += miliseconds;
 
-T.timerDiv.innerHTML = time;
+  T.timerDiv.innerHTML = time;
 }
 
 function startTimer() {
-// save start time
-T.timerStarted = startTime * 1000;
+  // save start time
+  T.timerStarted = startTime * 1000;
+  console.log('T.timerStarted: '+T.timerStarted)
 
-if (T.difference > 0) {
-T.timerStarted = T.timerStarted - T.difference
-}
-// update timer periodically
-T.timerInterval = setInterval(function() {
-displayTimer()
-}, 10);
-
-// show / hide the relevant buttons:
-document.getElementById('go').style.display="none";
-document.getElementById('stop').style.display="inline";
-document.getElementById('clear').style.display="none";
+  if (T.difference > 0) {
+    T.timerStarted = T.timerStarted - T.difference
+  }
+  // update timer periodically
+  T.timerInterval = setInterval(function() {
+    displayTimer()
+  }, 10);
 }
 
 function stopTimer() {
-clearInterval(T.timerInterval); // stop updating the timer
-
-document.getElementById('stop').style.display="none";
-document.getElementById('go').style.display="inline";
-document.getElementById('clear').style.display="inline";
-}
-
-function clearTimer() {
-clearInterval(T.timerInterval);
-T.timerDiv.innerHTML = "00:00:00:00";
-T.difference = 0;
-
-document.getElementById('stop').style.display="none";
-document.getElementById('go').style.display="inline";
-document.getElementById('clear').style.display="none";
-}
-
-// Run the timer if the race is in progress
-if (inProgress) {
-startTimer()
+  clearInterval(T.timerInterval); // stop updating the timer
 }
