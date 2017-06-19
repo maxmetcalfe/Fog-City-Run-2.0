147858a1dfe2d11debf4b72cf50df3798e888de0
Max Metcalfe <m.maxmetcalfe@gmail.com>
2017-06-18 19:57:05 -0700

    Finally adding some tests.

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -8,4 +8,12 @@ class Racer < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :race_count, numericality: { only_integer: true }
+  validates :race_count, numericality: { greater_than_or_equal_to: 0 }
+  validates :longest_streak, numericality: { only_integer: true }
+  validates :longest_streak, numericality: { :less_than_or_equal_to => :race_count }
+  validates :current_streak, numericality: { only_integer: true }
+  validates :current_streak, numericality: { :less_than_or_equal_to => :race_count }
+  validates :current_streak, numericality: { :less_than_or_equal_to => :longest_streak }
+  validates :fav_bib, numericality: { only_integer: true }
 end
--- test/models/racer_test.rb
+++ test/models/racer_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class RacerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save racer without name" do
+    racer = Racer.new
+    assert_not racer.save
+  end
+
+  test "should not have a negative race_count" do
+    racer = Racer.new
+    racer.first_name = "John"
+    racer.last_name = "Doe"
+    racer.race_count = -1
+    assert_not racer.save
+  end
+
+  test "should not save racer with a string race_count" do
+    racer = Racer.new
+    racer.first_name = "John"
+    racer.last_name = "Doe"
+    racer.race_count = "bad"
+    assert_not racer.save
+  end
+
+  test "should not save racer with a current streak longer than race count" do
+    racer = Racer.new
+    racer.first_name = "John"
+    racer.last_name = "Doe"
+    racer.race_count = 1
+    racer.longest_streak = 2
+    assert_not racer.save
+  end
+
+  test "should not save racer with a longest streak longer than race count" do
+    racer = Racer.new
+    racer.first_name = "John"
+    racer.last_name = "Doe"
+    racer.race_count = 3
+    racer.current_streak = 4
+    racer.longest_streak = 3
+    assert_not racer.save
+  end
+
+  test "should not save racer with a current streak longer than current_streak" do
+    racer = Racer.new
+    racer.first_name = "John"
+    racer.last_name = "Doe"
+    racer.race_count = 3
+    racer.current_streak = 3
+    racer.longest_streak = 2
+    assert_not racer.save
+  end
 end
