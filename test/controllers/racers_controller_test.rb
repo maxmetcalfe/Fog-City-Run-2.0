d631e706d8f8f314ae3568a0c1731ec218339741
Max Metcalfe <m.maxmetcalfe@gmail.com>
2017-12-22 14:25:12 -0800

    Improve test for longest streak.

--- test/controllers/racers_controller_test.rb
+++ test/controllers/racers_controller_test.rb
@@ -3,12 +3,14 @@ require 'test_helper'
 class RacersControllerTest < ActionController::TestCase
   test "longest streak matches reality" do
     # Max should have a longest streak of 2.
+    # Max has raced three races but only 2 in a "streak."
     longest_streak, streak = @controller.update_streak_calendar([1])
     assert_equal longest_streak.length, 2
   end
 
   test "current streak matches reality" do
     # Max should have a current streak of 0.
+    # TO DO: Improve this test for the case that Max has a streak.
     longest_streak, streak = @controller.update_streak_calendar([1])
     assert_equal streak.length, 0
   end
