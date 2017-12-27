02bfc6e9be37a9def76e80e37aa5a672c937ff9a
Max Metcalfe <m.maxmetcalfe@gmail.com>
2017-12-27 02:03:41 -0200

    Adding two Race model tests.

--- test/models/race_test.rb
+++ test/models/race_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class RaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save race without date" do
+    race = Race.new
+    assert_not race.save
+  end
+
+  test "should save race with valid date" do
+    race = Race.new
+    race.date = "2017-12-27"
+    assert race.save
+  end
 end
