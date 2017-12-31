bb2fb3af865344daa581f728dccf1fc0300594e4
Max Metcalfe <m.maxmetcalfe@gmail.com>
2017-12-31 14:27:27 -0400

    Move Coveralls call to config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Use Coveralls
+require 'coveralls'
+Coveralls.wear!
+
 # Initialize the Rails application.
 Rails.application.initialize!
