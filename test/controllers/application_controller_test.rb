772886b495c0b82dafb8860bcd73200bb2e8c657
Max Metcalfe <m.maxmetcalfe@gmail.com>
2018-01-26 21:24:25 -0800

    Add controller test for validate_ranks().

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -24,4 +24,13 @@ class ApplicationControllerTest < ActionController::TestCase
   test "to_seconds() returns the expected value - 1 hour" do
     assert_equal @controller.to_seconds("01:00:00.0"), 3600
   end
+
+  test "validate ranks maintains the correct rank order" do
+    @controller.validate_ranks(races(:one))
+    rank = 1
+    for result in races(:one).results
+      assert_equal result.rank, rank
+      rank += 1
+    end
+  end
 end
