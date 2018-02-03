c0ff66919486e3d6fd2d32001484ef16302dfac8
Max Metcalfe <m.maxmetcalfe@gmail.com>
2018-02-03 15:07:24 -0500

    Add some basic model tests for Order.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save a valid order" do
+    order = orders(:hat)
+    assert order.save
+  end
+
+  test "should not save an order without an item" do
+    order = orders(:hat)
+    order.item = ""
+    assert_not order.save
+  end
+
+  test "should not save an order with an invalid quantity" do
+    order = orders(:hat)
+    order.quantity = -1
+    assert_not order.save
+  end
+
+  test "should not save an order without a quantity" do
+    order = orders(:hat)
+    order.quantity = ""
+    assert_not order.save
+  end
+
+  test "should not save an order without a size" do
+    order = orders(:hat)
+    order.size = ""
+    assert_not order.save
+  end
+
+  test "should not save an order without a delivered attribute" do
+    order = orders(:hat)
+    order.delivered = ""
+    assert_not order.save
+  end
 end
