65ea086746bb9ad1bf279bf51bcc2b79980a17a1
Max Metcalfe <m.maxmetcalfe@gmail.com>
2018-02-03 11:12:58 -0500

    Adding basic tests for results.

--- test/controllers/results_controller_test.rb
+++ test/controllers/results_controller_test.rb
@@ -1,7 +1,25 @@
-require 'test_helper'
+require "test_helper"
 
 class ResultsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "result index displays basic result info" do
+    get :index
+    assert_select "p", count: 0
+  end
+
+  test "result show displays basic result info" do
+    result = results(:one)
+    get :show, id: result.id
+    assert_select "p", count: 6
+    assert_select "p:first-of-type", "Rank: " + result.rank.to_s
+    assert_select "p:last-of-type", "Race: " + result.race_id.to_s
+  end
+
+  test "destroying a result changes associated counts and redirects to race" do
+    result = results(:one)
+    race = Race.find(result.race_id)
+    delete :destroy, id: result.id
+    assert_equal 3, race.results.count
+    assert_redirected_to race
+  end
 end
