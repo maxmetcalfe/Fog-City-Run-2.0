5dba7bc30a3da195f81222955bc9b3075d4307e4
Max Metcalfe <m.maxmetcalfe@gmail.com>
2018-02-03 15:17:14 -0500

    Add basic model tests for Result.

--- app/models/result.rb
+++ app/models/result.rb
@@ -8,4 +8,12 @@ class Result < ActiveRecord::Base
   validates :group_name, presence: true
   validates :time, presence: true
   validates :race_id, presence: true
+  validates :bib, numericality: { only_integer: true }
+  validates :bib, numericality: { :greater_than_or_equal_to => 0 }
+  validates :rank, numericality: { only_integer: true }
+  validates :rank, numericality: { :greater_than_or_equal_to => 0 }
+  validates :racer_id, numericality: { only_integer: true }
+  validates :racer_id, numericality: { :greater_than_or_equal_to => 0 }
+  validates :race_id, numericality: { only_integer: true }
+  validates :race_id, numericality: { :greater_than_or_equal_to => 0 }
 end
--- test/models/result_test.rb
+++ test/models/result_test.rb
@@ -1,7 +1,56 @@
-require 'test_helper'
+require "test_helper"
 
 class ResultTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save a valid result" do
+    result = results(:one)
+    assert result.save
+  end
+
+  test "should not save a result without a racer" do
+    result = results(:one)
+    result.racer_id = ""
+    assert_not result.save
+  end
+
+  test "should not save a result without a race" do
+    result = results(:one)
+    result.race_id = ""
+    assert_not result.save
+  end
+
+  test "should not save a result without a time" do
+    result = results(:one)
+    result.time = ""
+    assert_not result.save
+  end
+
+  test "should not save a result without a rank" do
+    result = results(:one)
+    result.rank = ""
+    assert_not result.save
+  end
+
+  test "should not save a result with an invalid rank" do
+    result = results(:one)
+    result.rank = -1
+    assert_not result.save
+  end
+
+  test "should not save a result without a bib" do
+    result = results(:one)
+    result.bib = ""
+    assert_not result.save
+  end
+
+  test "should not save a result with an invalid bib" do
+    result = results(:one)
+    result.bib = -1
+    assert_not result.save
+  end
+
+  test "should not save a result without a group name" do
+    result = results(:one)
+    result.group_name = ""
+    assert_not result.save
+  end
 end
