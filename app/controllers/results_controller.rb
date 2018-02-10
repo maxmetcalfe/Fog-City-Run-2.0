9ecba3bc1f1471cceb600cc65712deb78e0f4c13
Max Metcalfe <m.maxmetcalfe@gmail.com>
2018-02-09 20:52:06 -0800

    Removing racer_id autocomplete hack for Result/New.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -38,19 +38,8 @@ class ResultsController < ApplicationController
     @result = Result.find(params[:id])
   end
 
-  # This is a Hack. We should be using a display_value for autocomplete. This is a temp implementation.
-  def modify_result_params
-    new_params = result_params
-    name_list = new_params[:racer_id].split
-    first_name = name_list[0]
-    last_name = name_list[1]
-    new_params[:racer_id] = Racer.where(first_name: first_name, last_name: last_name).first.id
-    return new_params
-  end
-
   def create
-    # Hack: See above at modify_result_params
-    @result = Result.new(modify_result_params)
+    @result = Result.new(result_params)
     @result.rank = 0
     @result.id = Result.maximum(:id).next
     update_racer_info([@result.racer_id])
