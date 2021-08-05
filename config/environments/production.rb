c9c9c252a507ab461d83e59a855ea47d520f8456
Max Metcalfe <hello@maxmetcalfe.com>
2021-08-05 07:11:30 -0400

    Update Prod host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'fogcityrun.com'
+  host = 'https://wwww.fogcityrun.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
