683735753279a91f92e7dcc8ff16b448c2a0f3b8
Max Metcalfe <hello@maxmetcalfe.com>
2021-08-05 07:50:01 -0400

    wwww -> www in Prod host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://wwww.fogcityrun.com'
+  host = 'https://www.fogcityrun.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
