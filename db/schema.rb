bd7504574c77924d02ba2fd7968242257e431324
Max Metcalfe <hello@maxmetcalfe.com>
2021-09-30 12:06:21 -0700

    Remove articles and comments from schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,23 +16,6 @@ ActiveRecord::Schema.define(version: 20210131214439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
-
   create_table "orders", force: :cascade do |t|
     t.string   "item"
     t.integer  "user_id"
