f1b2f66c17c52a95ccf47c585a57e79a194aac55
Max <hello@maxmetcalfe.com>
2026-01-07 07:58:39 -0600

    Have SaveRaceJob inherit from ActiveJob

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -110,30 +110,9 @@ class RacesController < ApplicationController
   # Save race
   def save_race
     race = Race.find(params[:id])
-    racer_ids = race.results.pluck(:racer_id)
-    streak_racer_ids = Racer.where("current_streak > 0 OR longest_streak > 0").pluck(:id)
-    racer_ids_to_update = (racer_ids + streak_racer_ids).uniq
-
-    race_counts = Result.where(racer_id: racer_ids_to_update).group(:racer_id).count
-    latest_bibs_by_racer = {}
-    Result.joins(:race)
-          .where(racer_id: racer_ids_to_update)
-          .order("results.racer_id ASC, races.date DESC")
-          .pluck("results.racer_id", "results.bib")
-          .each do |racer_id, bib|
-      latest_bibs_by_racer[racer_id] ||= bib
-    end
-
-    Racer.where(id: racer_ids_to_update).find_each do |racer|
-      racer.update_columns(
-        race_count: race_counts[racer.id] || 0,
-        fav_bib: latest_bibs_by_racer[racer.id]
-      )
-      update_streak_calendar(racer)
-    end
-
     race.update(state: 'FINISHED')
-    redirect_to race
+    SaveRaceJob.perform_later(race.id)
+    redirect_to race, notice: "Race save is running in the background."
   end
 
   # enable race.
--- config/application.rb
+++ config/application.rb
@@ -5,5 +5,6 @@ Bundler.require(*Rails.groups)
 module Blog
   class Application < Rails::Application
     config.load_defaults 6.1
+    config.active_job.queue_adapter = :async
   end
 end
