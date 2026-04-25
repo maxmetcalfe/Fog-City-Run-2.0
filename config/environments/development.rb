c3867f1566bc58a0e08507fba4ec6809cb159327
Max Metcalfe <hello@maxmetcalfe.com>
2026-04-25 16:09:03 -0300

    Add request UUID log tagging to production and development environments
    - Enable config.log_tags = [:uuid] to prepend UUID to all log lines
    - Makes it easier to correlate request/response/error logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Prepend all log lines with request UUID
+  config.log_tags = [ :uuid ]
 end
