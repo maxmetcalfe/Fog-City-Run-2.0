30da121497023d65026c2846350b03b69d5d028a
Max Metcalfe <hello@maxmetcalfe.com>
2026-04-28 14:28:17 -0300

    Add error handling and logging to password reset email flow

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,11 @@ class UserMailer < ApplicationMailer
 
   def password_reset(user)
     @user = user
+    logger.info "[UserMailer] Sending password reset email to: #{user.email}" if logger
     mail to: user.email, subject: "Password reset"
+  rescue => e
+    logger.error "[UserMailer ERROR] #{e.class}: #{e.message}" if logger
+    logger.error "[UserMailer ERROR] Backtrace: #{e.backtrace.join("\n")}" if logger && e.backtrace
+    raise e
   end
 end
