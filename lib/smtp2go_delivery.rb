ebc9aaa1997bf081c6a4e9343f46869c6d2e1b55
Max Metcalfe <hello@maxmetcalfe.com>
2026-04-29 13:38:59 -0300

    Fix SMTP2GO delivery - handle multipart emails and add logging

--- lib/smtp2go_delivery.rb
+++ lib/smtp2go_delivery.rb
@@ -9,15 +9,9 @@ class Smtp2goDelivery
   end
 
   def deliver!(mail)
-    payload = {
-      api_key: @api_key,
-      to: mail.to,
-      sender: mail.from.first,
-      subject: mail.subject,
-      text_body: mail.text_part&.body&.to_s,
-      html_body: mail.html_part&.body&.to_s
-    }.compact
-
+    payload = build_payload(mail)
+    Rails.logger.info "[SMTP2GO] Sending email with payload: #{payload.to_json}"
+    
     uri = URI(API_ENDPOINT)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -27,13 +21,46 @@ class Smtp2goDelivery
 
     response = http.request(request)
 
+    Rails.logger.info "[SMTP2GO] Response: #{response.code} #{response.message} - #{response.body}"
+
     unless response.is_a?(Net::HTTPSuccess)
-      raise "SMTP2GO delivery failed: #{response.code} #{response.message}"
+      raise "SMTP2GO delivery failed: #{response.code} #{response.message} - #{response.body}"
     end
 
     result = JSON.parse(response.body)
-    unless result['data'] && result['data']['succeeded'].to_i >= mail.to.size
+    unless result['data'] && result['data']['succeeded'].to_i > 0
       raise "SMTP2GO delivery failed: #{response.body}"
+    end
+  end
+
+  private
+
+  def build_payload(mail)
+    {
+      api_key: @api_key,
+      to: mail.to.map(&:to_s),
+      from: mail.from.first.to_s,
+      subject: mail.subject.to_s,
+      text_body: extract_text_body(mail),
+      html_body: extract_html_body(mail)
+    }.compact
+  end
+
+  def extract_text_body(mail)
+    if mail.multipart?
+      text_part = mail.parts.find { |p| p.content_type.match?(/text\/plain/) }
+      text_part ? text_part.body.to_s : nil
+    else
+      mail.content_type.match?(/text\/plain/) ? mail.body.to_s : nil
+    end
+  end
+
+  def extract_html_body(mail)
+    if mail.multipart?
+      html_part = mail.parts.find { |p| p.content_type.match?(/text\/html/) }
+      html_part ? html_part.body.to_s : nil
+    else
+      mail.content_type.match?(/text\/html/) ? mail.body.to_s : nil
     end
   end
 end
