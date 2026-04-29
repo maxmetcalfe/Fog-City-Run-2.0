78c3797d444f3ef594dcd574f00a5e1727b183f8
Max Metcalfe <hello@maxmetcalfe.com>
2026-04-29 12:35:09 -0300

    Fix SMTP2GO delivery method loading - require file explicitly

--- config/initializers/smtp2go.rb
+++ config/initializers/smtp2go.rb
@@ -1,4 +1,4 @@
 # Custom delivery method for SMTP2GO REST API
-# Autoloaded from lib/smtp2go_delivery.rb
+require Rails.root.join('lib/smtp2go_delivery')
 
 ActionMailer::Base.add_delivery_method(:smtp2go, Smtp2goDelivery)
