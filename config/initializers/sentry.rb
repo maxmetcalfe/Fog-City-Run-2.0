7299af3005602e14a06e420c2e7eb2c05978f702
Max Metcalfe <hello@maxmetcalfe.com>
2026-06-08 11:28:50 -0300

    Update Sentry config: add ENV guard, environment, release tracking, and PII setting

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,21 @@
-Sentry.init do |config|
-  config.dsn = ENV['SENTRY_DSN']
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.enabled_environments = %w[production]
-  config.traces_sample_rate = 0.5
+if ENV['SENTRY_DSN'].present?
+  Sentry.init do |config|
+    config.dsn = ENV['SENTRY_DSN']
+    config.environment = Rails.env
+
+    # Breadcrumbs configuration
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+    # Only enable in production
+    config.enabled_environments = %w[production]
+
+    # Sampling
+    config.traces_sample_rate = 0.5
+
+    # Release tracking
+    config.release = ENV['HEROKU_SLUG_COMMIT'] if ENV['HEROKU_SLUG_COMMIT']
+
+    # Send default PII (disable if you want to be more cautious)
+    config.send_default_pii = false
+  end
 end
