68d22a47509beee9ea9440816cd6b7f8f7cd3f1b
Max Metcalfe <hello@maxmetcalfe.com>
2026-06-08 10:43:57 -0300

    Fix Sentry initialization to only run when DSN is present

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,25 +1,21 @@
-Sentry.init do |config|
-  config.dsn = ENV['SENTRY_DSN']
-  config.environment = Rails.env
+if ENV['SENTRY_DSN'].present?
+  Sentry.init do |config|
+    config.dsn = ENV['SENTRY_DSN']
+    config.environment = Rails.env
 
-  # Breadcrumbs configuration
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    # Breadcrumbs configuration
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # Only enable in production
-  config.enabled_environments = %w[production]
+    # Only enable in production
+    config.enabled_environments = %w[production]
 
-  # Sampling
-  config.traces_sample_rate = 0.5
-  config.profiles_sample_rate = 0.1
+    # Sampling
+    config.traces_sample_rate = 0.5
 
-  # Release tracking
-  config.release = ENV['HEROKU_SLUG_COMMIT'] if ENV['HEROKU_SLUG_COMMIT']
+    # Release tracking
+    config.release = ENV['HEROKU_SLUG_COMMIT'] if ENV['HEROKU_SLUG_COMMIT']
 
-  # Send default PII (disable if you want to be more cautious)
-  config.send_default_pii = false
-
-  # Before send hook to filter sensitive data
-  config.before_send = lambda do |event, _hint|
-    event
+    # Send default PII (disable if you want to be more cautious)
+    config.send_default_pii = false
   end
 end
